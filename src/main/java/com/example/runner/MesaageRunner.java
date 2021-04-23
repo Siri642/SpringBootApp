6428fe342f8d8595add07661969480dcdc77d581
Sirisha <Sirisha@DESKTOP-NPQU4J9>
2021-04-23 07:47:31 -0400

    WH-8530-FIRST TIME FILE UPDATE

---
+++ src/main/java/com/example/runner/MesaageRunner.java
@@ -0,0 +1,14 @@
+package com.example.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MesaageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello welocmeiewfh");
+
+	}
+
+}
