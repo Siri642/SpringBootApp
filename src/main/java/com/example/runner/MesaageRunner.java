9b0fd92528f6d75862eda1883c046868d591a86e
Sirisha <Sirisha@DESKTOP-NPQU4J9>
2021-04-23 08:15:32 -0400

    wh-56-fourth

--- src/main/java/com/example/runner/MesaageRunner.java
+++ src/main/java/com/example/runner/MesaageRunner.java
@@ -1,5 +1,7 @@
 package com.example.runner;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
@@ -7,6 +9,7 @@ public class MesaageRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		org.slf4j.Logger log=LoggerFactory.getLogger(MesaageRunner.class);
 		int a=10;
 		System.out.println("hello welocmeiewfh");
 		
